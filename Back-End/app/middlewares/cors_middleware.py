ad21093ea54f7659224d052fe28c1d784e607eae
JEROLPOA2 <92282918+JEROLPOA2@users.noreply.github.com>
2025-01-25 10:30:12 -0500

    Requirements.txt

--- Back-End/app/middlewares/cors_middleware.py
+++ Back-End/app/middlewares/cors_middleware.py
@@ -7,7 +7,7 @@ class CORS:
     def add(app: FastAPI):
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:4200"],
+            allow_origins=["*"],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"]
